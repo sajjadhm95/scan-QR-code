30574eac4cd747b45dce922af26700c62c655ada
sajjadhm95 <124425319+sajjadhm95@users.noreply.github.com>
2023-09-10 09:25:42 +0330

    Add files via upload

---
+++ js.js
@@ -0,0 +1,38 @@
+'use strict'
+let i = Math.random()
+console.log(i)
+console.log(i * 6)
+console.log(Math.trunc(i * 6) + 1)
+let body = document.getElementsByTagName('body')
+document.getElementsByClassName('')
+ele.textContent = ''
+document.querySelector('')
+// let body = document.querySelector('body')
+
+body.innerhtml = '<h1 class="sal"> salam </h1>'
+let btnEl = document.querySelector('button')
+let imgEl = document.querySelector('img')
+let imgEl = document.querySelector('img')
+
+btnEl.addEventListener('click', () => {
+
+})
+
+imgEl.getAttribute()
+imgEl.setAttribute()
+imgEl.src = ''
+
+
+let clock = new Date().toLocaleTimeString()
+setInterval(() => {
+
+}, 1000);
+clearInterval()
+
+window.innerHeight
+window.innerWidth
+imgEl.getBoundingClientRect().top
+
+
+const arr = [2, 5, 9]
+const [l, j, k] = arr
